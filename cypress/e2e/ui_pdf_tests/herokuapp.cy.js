9b1646c84c16d8712652a06f465ea25312180f1c
Fábio Delazari Pereira <delazari.rj@gmail.com>
2025-01-19 22:55:42 -0300

    Adding HeroKuApp Test with PDF File Download

--- cypress/e2e/ui_pdf_tests/herokuapp.cy.js
+++ cypress/e2e/ui_pdf_tests/herokuapp.cy.js
@@ -1,21 +1,22 @@
 
 const path = require('path');
 
-describe('--', () => {
+describe('Knowledge of download and read a pdf file for the technical challenge', () => {
 
-    it('---', () => {
-        const fileName = '20250110_171832.pdf'
+    it('Download and read a pdf file', () => {
+        //const fileName = '20250110_171832.pdf'
+        const fileName = '1nt22cs049-db1.pdf'
 
         //Go to https://the-internet.herokuapp.com/download
         //Download the file 20250110_171832.pdf
-        cy.downloadFile('https://the-internet.herokuapp.com/download/{fileName}','downloads', fileName)
-
-        //Verify that the file has been downloaded with the correct name
-        const filePath = './downloads/{fileName}' 
-        let fileNameAUX = path.basename(filePath)
-        expect(fileNameAUX).to.be.equal('20250110_171832.pdf')
-
-        //
+        cy.request({
+            url: 'https://the-internet.herokuapp.com/download/' + fileName,
+            encoding: 'binary',
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            cy.writeFile('./downloads/' + fileName, response.body, 'binary')
+        })
+   
     })
 
 })
