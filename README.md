b6123d69b438c5316044ab72fa852c9fc481a03a
Fábio Delazari Pereira <delazari.rj@gmail.com>
2025-01-20 09:07:33 -0300

    adding test execution by the terminal and refactoring tests

---
+++ README.md
@@ -0,0 +1,6 @@
+1. Download do projeto https://github.com/delazari-rj/qa-interview-challenge-guilherme
+2. Descompactar em um diretório
+3. Instalar o nodeJS v22.13.0 (LTS) https://nodejs.org/en/download
+4. Executar o comando npm install para baixar as dependências
+5. Para rodar os testes via browser, utilizar o comando npm run cypress:open
+6. Para rodar os testes pelo console, utilizar o comando npm run cypress:run
--- cypress/e2e/ui_pdf_tests/herokuapp.cy.js
+++ cypress/e2e/ui_pdf_tests/herokuapp.cy.js
@@ -1,11 +1,7 @@
-
-const path = require('path');
-
 describe('Knowledge of download and read a pdf file for the technical challenge', () => {
 
     it('Download and read a pdf file', () => {
-        //const fileName = '20250110_171832.pdf'
-        const fileName = '1nt22cs049-db1.pdf'
+        const fileName = '20250110_171832.pdf'
 
         //Go to https://the-internet.herokuapp.com/download
         //Download the file 20250110_171832.pdf
