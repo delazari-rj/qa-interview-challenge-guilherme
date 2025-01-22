a02bd27b0b13dac4a531696b9809be0d3e1d2e3c
Fábio Delazari Pereira <delazari.rj@gmail.com>
2025-01-22 10:09:03 -0300

    fixing download file and adding verify if file have been downloaded

--- cypress/e2e/ui_pdf_tests/herokuapp.cy.js
+++ cypress/e2e/ui_pdf_tests/herokuapp.cy.js
@@ -1,7 +1,8 @@
 describe('Knowledge of download and read a pdf file for the technical challenge', () => {
 
+
     it('Download and read a pdf file', () => {
-        const fileName = '20250110_171832.pdf'
+        const fileName = 'testing.pdf'
 
         //Go to https://the-internet.herokuapp.com/download
         //Download the file 20250110_171832.pdf
@@ -12,7 +13,14 @@ describe('Knowledge of download and read a pdf file for the technical challenge'
             expect(response.status).to.eq(200)
             cy.writeFile('./downloads/' + fileName, response.body, 'binary')
         })
-   
+
+        //Verify that the file has been downloaded with the correct name
+        cy.readFile('./downloads/testing.pdf')
+            .should('exist')
+        
+        //Check if the name "Roshan B Varghese" exists as content in the PDF.
+
+        //Check if in the Skill Set section, you have experience in "Java Script".
     })
 
 })
